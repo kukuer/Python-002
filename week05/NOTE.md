145a2f642d78818927905ccbf5ae973b05fb0d07
wangxiaohui <wangxiaohui_cn@163.com>
2020-09-06 23:55:39 +0800

    第7周作业

--- week05/NOTE.md
+++ week05/NOTE.md
@@ -1,1 +1,37 @@
-学习笔记
+# Django学习笔记
+
+**官方文档**
+
+ [ttps://docs.djangoproject.com/zh-hans/3.0/](https://docs.djangoproject.com/zh-hans/3.0/) 
+
+1. 创建django项目：django-admin startproject  <project_name>
+
+2. 目录结构：
+
+   ```python
+   MyDjango:
+       manage.py   #命令行工具，管理django项目
+       MyDjango
+       	__init__.py
+           setting.py 		#项目配置文件
+           urls.py			
+           wsgi.py
+   ```
+
+3. 创建Django应用程序
+
+   python manage.py help	#查看工具具体功能
+
+   python manage.py startapp index 	#创建应用程序
+
+   ```python
+   index
+   	migrations	#数据库迁移文件夹
+       models.py	#模型
+       apps.py		#当前app配置文件
+       admin.py	#管理后台
+       tests.py	#自动化测试
+       views.py	#视图
+   ```
+
+   
---
+++ week07/job.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+from abc import ABCMeta, abstractmethod
+
+
+class Animal(metaclass=ABCMeta):
+
+    @property
+    def animal_type(self):
+        return self.type
+
+    @animal_type.setter
+    def animal_type(self, value):
+        type = ('食肉', '食草')
+        if value not in type:
+            raise ValueError(f'{value} type not found {type}')
+        self.type = value
+
+    @property
+    def animal_physical(self):
+        return self.physical
+
+    @animal_physical.setter
+    def animal_physical(self, value):
+        physical = ('大型', '中型', '小型')
+        if value not in physical:
+            raise ValueError(f'{value} physical not found {physical}')
+        self.physical = value
+
+    @property
+    def animal_character(self):
+        return self.physical
+
+    @animal_character.setter
+    def animal_character(self, value):
+        character = ('温顺', '凶猛')
+        if value not in character:
+            raise ValueError(f'{value} character not found {character}')
+        self.character = value
+
+    def is_preyer(self):
+        if (
+                self.type == '食肉'
+                and self.physical != '小型'
+                and self.character == '凶猛'
+        ):
+            return True
+        return False
+
+    @abstractmethod
+    def is_pet(self):
+        pass
+
+
+def singleton(cls):
+    instances = {}
+    def get_instance():
+        if cls not in instances:
+            instances[cls] = cls()
+        return instances[cls]
+    return get_instance
+
+
+@singleton
+class Cat(Animal):
+    sound = '喵喵喵'
+
+    def __init__(self, name, type, physical, character):
+        self.name = name
+        self.pet = True
+        self.animal_type = type
+        self.animal_physical = physical
+        self.animal_character = character
+
+    def is_pet(self):
+        return not self.is_preyer()
+
+
+@singleton
+class Dog(Animal):
+    sound = '汪汪汪'
+
+    def __init__(self, name, type, physical, character):
+        self.name = name
+        self.pet = True
+        self.animal_type = type
+        self.animal_physical = physical
+        self.animal_character = character
+
+    def is_pet(self):
+        return not self.is_preyer()
+
+
+class Zoo(object):
+
+    def __init__(self, name):
+        self.name = name
+        self.zoo_animal = []
+
+    def add_animal(self, animal):
+        class_name = type(animal).__name__
+        if class_name in self.__dict__:
+            raise ValueError(f'{animal}动物园中已经存在')
+        self.__dict__[class_name] = True
+
+
+if __name__ == '__main__':
+    cat = Dog('大花猫', '食肉', '小型', '凶猛')
+    print(cat.sound)
+    print(cat.name)
+    print(f'是否适合当宠物: {cat.is_pet()}')
